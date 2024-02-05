1a788476bb2d648fd7d826b7242924a8a1889895
yallappa jogi <yallappajogi43@gmail.com>
2024-02-05 12:34:49 +0530

    code for square and cube functionalities

--- calculator.java
+++ calculator.java
@@ -1,32 +1,48 @@
 public class calculator {
     float num1;
     float num2;
+
     calculator(float num1, float num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
+
     float add() {
-        return num1+num2;
+        return num1 + num2;
     }
+
     float subtract() {
-        return num1-num2;
+        return num1 - num2;
     }
+
     float multiply() {
-        return num1*num2;
+        return num1 * num2;
     }
+
     float divide() {
-        if (num2!= 0) {
-            return num1/num2;
+        if (num2 != 0) {
+            return num1 / num2;
         } else {
             System.out.println("Error: Division by zero is not allowed.");
             return num1;
         }
     }
+
+    float square() {
+        return num1 * num1;
+    }
+
+    float cube() {
+        return num1 * num1 * num1;
+    }
+
     public static void main(String[] args) {
         calculator calculator = new calculator(10, 5);
         System.out.println("Addition: " + calculator.add());
         System.out.println("Subtraction: " + calculator.subtract());
         System.out.println("Multiplication: " + calculator.multiply());
         System.out.println("Division: " + calculator.divide());
+        System.out.println("Square: " + calculator.square());
+        System.out.println("Cube: " + calculator.cube());
     }
 }
